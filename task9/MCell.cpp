6e120ccfdba1cd34ef67b5a5b4f69821261a8ebb
Aidar <Sharipow777@gmail.com>
2020-12-22 22:12:42 +0300

    task9 deleted from task10 branch

--- task9/MCell.cpp
+++
@@ -1,5 +0,0 @@
-#include "MCell.h"
-bool MCell::right() const { return m_right; }
-bool MCell::down() const { return m_down; }
-void MCell::set_right(bool boolean) { m_right = boolean; }
-void MCell::set_down(bool boolean) { m_down = boolean; }

771fbd8d05eace368627d62db10c7c11af22623f
Aidar <Sharipow777@gmail.com>
2020-12-23 14:27:09 +0300

    исправления

--- task10/main.cpp
+++ task10/main.cpp
@@ -6,12 +6,16 @@
 #include <vector>
 #include <tuple>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
 void buildFullMaze(Maze& iMaze, MTreeNode& tree) {
-	const  int size = 31;
-	int distances[size][size];
+	int max = 0, average = 0;
+	auto distances = new int*[iMaze.rows + 1];
+	for (int z = 0; z < iMaze.rows + 1; z++) {
+		distances[z] = new int[iMaze.rows + 1];
+	}
 	vector<MTreeNode*> stack;
 	std::random_device rd;     
 	std::mt19937 rng(rd());    
@@ -26,15 +30,15 @@ void buildFullMaze(Maze& iMaze, MTreeNode& tree) {
 	MTreeNode& root = tree;
 	auto stuck = 0;
 	while (true) {
-		int tries = 0;
+		int attempts = 0;
 		while (true) {
-			if (tries > 40) {
+			if (attempts > 40) {
 				if (stuck == 1500 || indexroot >= stack.size())
 					break;
 				i = stack[indexroot]->i();
 				j = stack[indexroot]->j();
 				root = *stack[indexroot];
-				tries = 0;
+				attempts = 0;
 				indexroot++;
 				stuck++;
 				
@@ -54,7 +58,7 @@ void buildFullMaze(Maze& iMaze, MTreeNode& tree) {
 					if (!finded)
 						break;
 				}
-				tries++;
+				attempts++;
 			}			
 		}
 		if (stuck == 1500 || indexroot >= stack.size()) break;
@@ -63,14 +67,17 @@ void buildFullMaze(Maze& iMaze, MTreeNode& tree) {
 		j = j + randY;
 		root.addChild(i, j);
 		distances[i][j] = root.distance();
+		if (root.distance() > max)
+			max = root.distance();
+		average += root.distance();
 		stack.push_back(root.hasChild(i, j));
 		root = *root.hasChild(i, j);	
 	}
 
 
-	for (auto i = 0; i < 30; i++)
+	for (auto i = 0; i < iMaze.rows; i++)
 	{
-		for (auto k = 0; k < 30; k++)
+		for (auto k = 0; k < iMaze.rows; k++)
 		{
 			if (distances[i][k] < 10)
 				cout << distances[i][k] << "   ";
@@ -83,26 +90,41 @@ void buildFullMaze(Maze& iMaze, MTreeNode& tree) {
 		}
 		cout << endl;
 	}
+	cout << "\n\n\n" << endl;
+	cout << "max weight is: " << max << endl;
+	cout << "average weight is:  " << average / (iMaze.rows * iMaze.rows + 0.0f) << endl;
+	cout << "\n\n\n" << endl;
 }
 
 int main()
 {
-	const int rows = 30;
-	const int columns = 30;
-	Maze maze(rows,columns);
+	string read;
+	int size;
+	cout << "Enter size of labyrinth: ";
+	cin >> read ;
+	while (true) {
+		try {
+			size = stoi(read);
+			break;
+		}
+		catch(invalid_argument arg) {
+			cout << "Wrong input, try again" << endl;
+			cin >> read;
+		}
+	}
+	Maze maze(size, size);
 	std::random_device rd;
 	std::mt19937 rng(rd());
-	std::uniform_int_distribution<int> uni(-1, 1);
-	std::uniform_int_distribution<int> i_uni(0, rows);
-	std::uniform_int_distribution<int> j_uni(0, columns);
-	int i = i_uni(rng);
-	int j = j_uni(rng);
-	while (j != 0 && j != columns && i != 0 && i != rows)
+	std::uniform_int_distribution<int> uni(0, size);
+	int i = uni(rng);
+	int j = uni(rng);
+	while (j != 0 && j != size && i != 0 && i != size)
 	{
-		i = i_uni(rng);
-		j = j_uni(rng);
+		i = uni(rng);
+		j = uni(rng);
 	}
 	MTreeNode* tree = MTreeNode::beginTree(i,j);
+	cout << "\n\n\n";
 	buildFullMaze(maze, *tree);
 	maze.printMaze();
 }
